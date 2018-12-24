1b06fef15f176ca9f6213bade3f593bb5851219a
Damian <damianmdst@gmail.com>
2018-12-24 18:23:38 -0300

    Add react-native-elements and searchbar to main screen

--- src/App.js
+++ src/App.js
@@ -6,14 +6,13 @@
  * @flow
  */
 
-import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import React, { Component } from 'react';
+import { Platform, StyleSheet, Text, View } from 'react-native';
+import { SearchBar } from 'react-native-elements';
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
+  android: 'Double tap R on your keyboard to reload,\n' + 'Shake or press menu button for dev menu',
 });
 
 type Props = {};
@@ -21,9 +20,16 @@ export default class App extends Component<Props> {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
+        <SearchBar
+          containerStyle={{
+            width: '100%',
+            height: 46,
+            backgroundColor: 'white',
+            borderTopWidth: 0,
+          }}
+          lightTheme
+          noIcon
+          placeholder='Pokedex' />
       </View>
     );
   }
