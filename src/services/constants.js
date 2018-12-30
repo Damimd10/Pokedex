df353a6a52a90c9c78ea2374a96e60683cdd8fdc
Damian <damianmdst@gmail.com>
2018-12-29 23:02:12 -0300

    Create material palette color for species

--- src/services/constants.js
+++ src/services/constants.js
@@ -1,4 +1,47 @@
 const BASE_URL = 'https://pokeapi.co/api/v2/pokemon/';
 const BASE_SPRITE_URL = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/';
 
-export { BASE_SPRITE_URL, BASE_URL };
+const PALETTE_COLOR = {
+  blue: {
+    dark: '#1976D2',
+    light: '#BBDEFB',
+    primary: '#2196F3',
+  },
+  brown: {
+    dark: '#5D4037',
+    light: '#D7CCC8',
+    primary: '#795548',
+  },
+  gray: {
+    dark: '#616161',
+    light: '#F5F5F5',
+    primary: '#9E9E9E',
+  },
+  green: {
+    dark: '#388E3C',
+    light: '#C8E6C9',
+    primary: '#4CAF50',
+  },
+  pink: {
+    dark: '#C2185B',
+    light: '#F8BBD0',
+    primary: '#E91E63',
+  },
+  purple: {
+    dark: '#7B1FA2',
+    light: '#E1BEE7',
+    primary: '#9C27B0',
+  },
+  red: {
+    dark: '#D32F2F',
+    light: '#FFCDD2',
+    primary: '#F44336',
+  },
+  yellow: {
+    dark: '#FBC02D',
+    light: '#FFF9C4',
+    primary: '#FFEB3B',
+  },
+};
+
+export { BASE_SPRITE_URL, BASE_URL, PALETTE_COLOR };
