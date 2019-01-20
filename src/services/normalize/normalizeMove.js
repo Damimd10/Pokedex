e7ce8f2bb1113a24d40110841fce88cf1bccdb4b
Damian <damianmdst@gmail.com>
2019-01-20 20:38:47 -0300

    Add default values for move set

--- src/services/normalize/normalizeMove.js
+++ src/services/normalize/normalizeMove.js
@@ -2,9 +2,9 @@ const formatName = name => name.replace(/-/g, ' ');
 
 const normalizeMove = moves => ({
   name: formatName(moves.name),
-  accuracy: moves.accuracy,
-  power: moves.power,
-  pp: moves.pp,
+  accuracy: moves.accuracy || 0,
+  power: moves.power || 0,
+  pp: moves.pp || 0,
 });
 
 export default normalizeMove;
