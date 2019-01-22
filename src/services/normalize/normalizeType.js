10e40f97e36c8c76401e91b498c51e3f09f17129
Damian <damianmdst@gmail.com>
2019-01-21 21:35:19 -0300

    Add flow eslint configuration

--- src/services/normalize/normalizeType.js
+++ src/services/normalize/normalizeType.js
@@ -3,9 +3,10 @@ import { TYPES_COLOR } from '../constants';
 const addColor = damageArray =>
   damageArray.map(data => ({ ...data, color: TYPES_COLOR[data.name] }));
 
-const normalizeType = ({ damage_relations: damageRelations }) => ({
-  damageTo: addColor(damageRelations.double_damage_to),
-  damageFrom: addColor(damageRelations.double_damage_from),
-});
+const normalizeType = props =>
+  console.log('HERE', props) || {
+    damageTo: addColor(props.damageRelations.double_damage_to),
+    damageFrom: addColor(props.damageRelations.double_damage_from),
+  };
 
 export default normalizeType;
