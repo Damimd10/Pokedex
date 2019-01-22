301914e318f89e55ca910d3142964782fd5147fa
Damian <damianmdst@gmail.com>
2019-01-21 22:41:25 -0300

    Add first type file for getType service

--- src/services/getType.js
+++ src/services/getType.js
@@ -1,12 +1,16 @@
-import axios from 'axios';
+// @flow
 
+import axios from 'axios';
+import type { AxiosPromise } from 'axios';
+
+import { ITypes } from './models';
 import { normalizeType } from './normalize';
 import { handleErrorsResponse } from './utils';
 
-const getType = url =>
+const getType = (url: string): AxiosPromise<ITypes> =>
   axios
     .get(url)
-    .then(response => response.data)
+    .then((response: object) => response.data)
     .then(normalizeType)
     .catch(handleErrorsResponse);
 
